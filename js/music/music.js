8ef438825e68da7e0fe1351f7bb806085d3d6a6e
Antu <antukhadem17@gmail.com>
2022-07-02 17:32:02 +0600

    initial commit

---
+++ js/music/music.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const audio = document.querySelector('#audio');
---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$('.slider-container').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+});
